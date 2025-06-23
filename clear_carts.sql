61e2eba2c66f8a06aec305518a7bef82769e2777
dungph56212 <tiendung08102005@gmail.com>
2025-06-23 16:28:14 +0700

    delete

--- clear_carts.sql
+++
@@ -1,9 +0,0 @@
--- XÓA DỮ LIỆU GIỎ HÀNG CŨ
--- Chạy câu lệnh này trên phpMyAdmin
-
-TRUNCATE TABLE carts;
-
--- Kiểm tra bảng đã trống chưa
-SELECT COUNT(*) as total_carts FROM carts;
-
--- Kết quả phải là 0
